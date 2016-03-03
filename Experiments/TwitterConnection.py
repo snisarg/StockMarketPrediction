3b89b3c8bd94c5597e9d7b1da029b70fa61f82f7
Nisarg Shah <snisarg@gmail.com>
2016-03-02 23:04:58 -0800

    Twitter timeline plus gitignore added

--- Experiments/TwitterConnection.py
+++ Experiments/TwitterConnection.py
@@ -1,5 +1,10 @@
 import twitter
 
-api = twitter.Api(consumer_key='6u5reXpW9MG6reYgGceVzsDLM', consumer_secret='I6w4MQjvkpUTxTxXtUTAwVi00tguWui7p1xgO3osvPSuVqkGV4',
-	access_token_key='41324420-1XD5E5aO9pAssAo7mpssB8cTMGMzfQoz0dXBm239j', access_token_secret='5aSVkHVNeRd3VyKZU4fqGHSNo2Uif4VahvxhTLgq2HCGF')
-print api.VerifyCredentials()
+api = twitter.Api(consumer_key='6u5reXpW9MG6reYgGceVzsDLM',
+                  consumer_secret='I6w4MQjvkpUTxTxXtUTAwVi00tguWui7p1xgO3osvPSuVqkGV4',
+                  access_token_key='41324420-1XD5E5aO9pAssAo7mpssB8cTMGMzfQoz0dXBm239j',
+                  access_token_secret='5aSVkHVNeRd3VyKZU4fqGHSNo2Uif4VahvxhTLgq2HCGF')
+print api.GetUser("business")
+statuses = api.GetUserTimeline("@business")
+print [s.text for s in statuses]
+# print api.VerifyCredentials()
