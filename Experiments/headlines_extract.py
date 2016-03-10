6ec739053719858f9efcff84121da75465d12c46
Nisarg Shah <snisarg@gmail.com>
2016-03-10 11:54:47 -0800

    file importing and timestamps

---
+++ Experiments/headlines_extract.py
@@ -0,0 +1,12 @@
+import pandas
+import numpy
+
+headlines = pandas.read_csv('../data/headlines_20.txt', parse_dates=[0])
+print headlines.dtypes
+# print headlines.loc[0][0].date()
+
+goog_stock = pandas.read_csv('../data/Google.csv', parse_dates=[0])
+goog_stock = goog_stock[['Date', 'Close', 'Volume', 'Threshold Change']] # Columns we are extracting. Add more here.
+#print goog_stock
+
+print goog_stock[goog_stock['Threshold Change'] == '1']
