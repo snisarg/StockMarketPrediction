10e3628f6ec13e3793a0342694a1302a44ed3efb
Nisarg Shah <snisarg@gmail.com>
2016-03-10 12:09:42 -0800

    File name changes

--- Experiments/headlines_extract.py
+++ Experiments/headlines_extract.py
@@ -5,7 +5,7 @@ headlines = pandas.read_csv('../data/headlines_20.txt', parse_dates=[0])
 print headlines.dtypes
 # print headlines.loc[0][0].date()
 
-goog_stock = pandas.read_csv('../data/Google.csv', parse_dates=[0])
+goog_stock = pandas.read_csv('../data/google.csv', parse_dates=[0])
 goog_stock = goog_stock[['Date', 'Close', 'Volume', 'Threshold Change']] # Columns we are extracting. Add more here.
 #print goog_stock
 
