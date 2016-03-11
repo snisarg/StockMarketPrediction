b46f47aaa24389d5e5ed2b37b981c28ef38ee595
Nisarg Shah <snisarg@gmail.com>
2016-03-10 23:14:42 -0800

    improved utility

--- utility.py
+++ utility.py
@@ -1,15 +1,25 @@
 import pandas
 
-headlines = pandas.read_csv('../data/headlines.txt', parse_dates=[0])
+headlines = pandas.read_csv('data/headlines.txt', parse_dates=[0])
 headlines = headlines[headlines['message'].notnull()]
 
 
 def get_prices(stock_name):
     stock = pandas.read_csv('data/'+stock_name+'.csv', parse_dates=[0])
-    stock = stock[['Date', 'Close', 'Volume', 'Threshold Change', 'Next day']] # Columns we are extracting. Add more here.
-    return stock[stock['Threshold Change'] == '1']
+    stock = stock[['Date', 'Close', 'Volume', 'Threshold Change', 'Next day', 'Price change']]
+    # Columns we are extracting. Add more here.
+    return stock
+
+
+def get_prices_threshold(dataframe):
+    return dataframe[dataframe['Threshold Change'] == '1']
 
 
 def headlines_for(topic):
     return headlines[headlines['message'].str.contains(topic, case=False)]
+
+
+def get_feature_vector(sentence):
+    # Feature extraction code here.
+    return 'result'
 
