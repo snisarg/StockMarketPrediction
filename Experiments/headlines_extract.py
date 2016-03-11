2e8a5ac995bed345386d3584928ff0a27e3adc0d
Nisarg Shah <snisarg@gmail.com>
2016-03-11 10:07:56 -0800

    matches headlines and prices

--- Experiments/headlines_extract.py
+++ Experiments/headlines_extract.py
@@ -10,12 +10,13 @@ headlines = headlines[headlines['message'].notnull()]
 goog_stock = pandas.read_csv('../data/google.csv', parse_dates=[0])
 goog_stock = goog_stock[['Date', 'Close', 'Volume', 'Threshold Change', 'Next day']]
 # Columns we are extracting. Add more here.
-# print goog_stock
+# print goog_stock.dtypes
 
-change_dates = goog_stock[goog_stock['Threshold Change'] == '1']
-
+# change_dates = goog_stock[goog_stock['Threshold Change'] == '1']
+# print headlines.dtypes
 # print headlines[headlines['msg_dt'].date() == change_dates and 'Google' in headlines['message']]
-headlines['msg_dt_only'] = headlines.msg_dt.map(lambda x: x.date)
+
+headlines['msg_dt_only'] = headlines.msg_dt.map(pandas.datetools.normalize_date)
 # print headlines.head()
 
 # dates_only = headlines[(headlines['msg_dt_only'] in change_dates)]
@@ -24,8 +25,15 @@ headlines['msg_dt_only'] = headlines.msg_dt.map(lambda x: x.date)
 #print headlines[headlines['message'].isnull()]
 
 relevant_new = headlines[headlines['message'].str.contains('microsoft', case=False)]
-print relevant_new.shape
+print 'News size : {}'.format(relevant_new.shape)
 
+joint = pandas.merge(relevant_new, goog_stock, left_on='msg_dt_only', right_on='Date')
+print joint
+joint.to_csv('../data/joint_data.csv', sep='\t')
+'''
+The number of merged news points are less than news articles since some are published on non working days.
+Best case would be to pick a future price, left for later
+'''
 
 
 # merged = pandas.merge(left=change_dates, right=headlines)
