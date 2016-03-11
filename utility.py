fc61d5ce9445cefc135b0e92047f0394e489aa10
Nisarg Shah <snisarg@gmail.com>
2016-03-11 15:38:35 -0800

    Methods for word extractions and ML

--- utility.py
+++ utility.py
@@ -7,15 +7,30 @@ from nltk.stem.lancaster import LancasterStemmer
 import os
 from itertools import dropwhile
 import posttagger
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
+import re
+from sklearn import feature_extraction
+import string
+import numpy
+import os.path
 
 punkt = nltk.tokenize.punkt.PunktSentenceTokenizer()
-headlines = pandas.read_csv('data/headlines.txt', parse_dates=[0])
-headlines = headlines[headlines['message'].notnull()]
-headlines['Date'] = headlines.msg_dt.map(pandas.datetools.normalize_date)
+__stemmer = nltk.stem.LancasterStemmer()
+__words_only = re.compile("^[A-Za-z]*$")
+
+
+def get_headlines():
+    headlines = pandas.read_csv('data/headlines.txt', parse_dates=[0], encoding='latin_1')
+    headlines = headlines[headlines['message'].notnull()]
+    headlines['Date'] = headlines.msg_dt.map(pandas.datetools.normalize_date)
+    return headlines
 
 
 def get_prices(stock_name):
     stock = pandas.read_csv('data/'+stock_name+'.csv', parse_dates=[0])
+    stock['Threshold Change'] = stock['Threshold Change'].replace(' ', 0).fillna(0.0).astype(float)
     stock = stock[['Date', 'Close', 'Volume', 'Threshold Change', 'Next day', 'Price change']]
     # Columns we are extracting. Add more here.
     return stock
@@ -26,13 +41,59 @@ def get_prices_threshold(dataframe):
 
 
 def headlines_for(topic):
+    headlines = get_headlines()
     return headlines[headlines['message'].str.contains(topic, case=False)]
 
 
 def get_news_prices(company):
-    # The number of merged news points are less than news articles since some are published on non working days.
-    # Best case would be to pick a future price, left for later. Doing only 'inner' join for now.
-    return pandas.merge(headlines_for(company), get_prices(company), left_on='Date', right_on='Date')
+    if os.path.isfile('data/'+company+'_news.csv'):
+        return pandas.read_csv('data/'+company+'_news.csv', parse_dates=[1, 3], encoding='latin_1')
+    else:
+        # The number of merged news points are less than news articles since some are published on non working days.
+        # Best case would be to pick a future price, left for later. Doing only 'inner' join for now.
+        data = pandas.merge(headlines_for(company), get_prices(company), left_on='Date', right_on='Date')
+        return data.to_csv('data/'+company+'_news.csv', encoding='latin_1')
+
+
+def __punctuation_cleaner(s):
+    if s not in string.punctuation:
+        return True
+    return False
+
+
+def __stop_word_cleaner(s):
+    if s not in feature_extraction.text.ENGLISH_STOP_WORDS:
+        return True
+    return False
+
+
+def __stem_cleaner(s):
+    return __stemmer.stem(s)
+
+
+def __regex_filter(s):
+    if __words_only.match(s) is not None:
+        return True
+    return False
+
+
+def __clean_word(s):
+    result = ""
+    if s is not None:
+        for w in nltk.tokenize.word_tokenize(s.lower()):
+            #print w
+            if w is not None and __stop_word_cleaner(w) and __punctuation_cleaner(w) and __regex_filter(w):
+                result += " " + __stem_cleaner(w)
+    #print result
+    return result
+
+
+def pipeline_setup(learning_algo):
+    tf_idf = TfidfVectorizer(preprocessor=__clean_word, use_idf=True)
+    lsa = TruncatedSVD(n_components=50, n_iter=5, random_state=25)
+    pipeline_list = [('tf_idf', tf_idf), ('svd', lsa), ('learning_algo', learning_algo)]
+    pipeline = Pipeline(pipeline_list)
+    return pipeline
 
 
 def passivep(tags):
