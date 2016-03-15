c740ac20dece9f428fe3a4276aa6f4df63e74c0b
Nisarg Shah <snisarg@gmail.com>
2016-03-15 08:39:55 -0700

    Separated NN and ML files

--- nn_techniques.py
+++ nn_techniques.py
@@ -15,37 +15,33 @@ goog.append(utility.get_news_prices('adobe'))
 goog.append(utility.get_news_prices('ford'))
 
 # Select model of computation:
-# model = neural_network.MLPRegressor([len(verbnet.classids()), 200, 8], 'relu', 'adam', 0.0001, 200, 'constant', 0.001, 0.5, 200,
-#                                      True, None, 0.0001, False, False, 0.9, True, False, 0.1, 0.9, 0.999, 1e-08)
-
-model = RandomForestRegressor(n_estimators=50, max_features=30, max_depth=9, n_jobs=1)
+model = neural_network.MLPRegressor([len(verbnet.classids()), 200, 8], 'relu', 'adam', 0.0001, 200, 'constant', 0.001, 0.5, 200,
+                                     True, None, 0.0001, False, False, 0.9, True, False, 0.1, 0.9, 0.999, 1e-08)
+# model = RandomForestRegressor(n_estimators=50, max_features=30, max_depth=9, n_jobs=1)
 # model = SVC(kernel='linear', probability=True, random_state=40)
 # model = linear_model.LinearRegression()
 
-model = utility.pipeline_setup(model)
+# model = utility.pipeline_setup(model)
 
 # model_fitted = model.fit(goog['message'], goog['Threshold Change'])
 
 # Select columns:
-#x = goog.message.apply(lambda sentence: utility.get_feature_vector(sentence+".")[0])
+x = goog.message.apply(lambda sentence: utility.get_feature_vector(sentence+".")[0])
 # x.to_csv('data/google_msg_id.csv')
 # x = pandas.read_csv('data/google_msg_id.csv')
 # print x
-x = goog['message']
-#x = x.apply(lambda i: utility.one_hot(i))
-# array = numpy.zeros((len(x), len(verbnet.classids())))
-# for results in range(len(x)):
-#     for i in x[results]:
-#         # print i
-#         array[results][i] = 1
+# x = goog['message']
+# x = x.apply(lambda i: utility.one_hot(i))
+array = numpy.zeros((len(x), len(verbnet.classids())))
+for results in range(len(x)):
+    for i in x[results]:
+        # print i
+        array[results][i] = 1
 
-#x = x['[0]'].apply(lambda i: utility.one_hot(i))
-#x = numpy.matrix(x)
 # print array.shape
-#x = array
+x = array
 # print array
 #x=numpy.reshape(x,(len(x), (len(verbnet.classids()))))
-
 
 # y = goog['Threshold Change'].astype(int)
 y = goog['Direction']
